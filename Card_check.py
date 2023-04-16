e5fafe56b13c90eea8fc687e594817906d9e3985
MichalEder <edermichal.eder@gmail.com>
2023-04-16 12:39:58 +0200

    suma sude

--- Card_check.py
+++ Card_check.py
@@ -4,3 +4,10 @@ def prevrat(cislo_karty):
 def suma_liche_pozice(cislo_karty_rev):
     return sum([int(cislo) for i, cislo in enumerate(karta_rev) if (i+1)%2])
 
+def suma_sude_pozice(cislo_karty):
+    sude_pozice = [cislo for i, cislo in enumerate(karta_rev) if not (i+1)%2]
+    nasobky = [str(int(nasobek)*2) for nasobek in sude_pozice]
+    suma = 0
+    for i in "".join(nasobky):
+        suma += int(i)
+    return suma 
