a3b5e28ca04a13d6772bc6a3377c0838dd830944
MichalEder <edermichal.eder@gmail.com>
2023-04-16 12:30:56 +0200

    def sum odd positions

--- Card_check.py
+++ Card_check.py
@@ -1,2 +1,6 @@
 def prevrat(cislo_karty):
     return [x for x in cislo_karty[::-1]]
+
+def suma_liche_pozice(cislo_karty_rev):
+    return sum([int(cislo) for i, cislo in enumerate(karta_rev) if (i+1)%2])
+
